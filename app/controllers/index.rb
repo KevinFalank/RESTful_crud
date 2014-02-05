a222e757615eca3f0a19d292578bcab231d50b74
Kevin Falank <kevinf@carolina.rr.com>
2014-02-04 21:05:43 -0600

    Add all functinality for MVP

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,16 +1,38 @@
 get '/' do
-  # Look in app/views/index.erb
+  @notes = Note.all
   erb :index
 end
 
 get '/notes/new' do
-
+	erb :new
 end
 
-post '/notes' do
-
+post '/notes/new' do
+	@note = params[:note]
+	Note.create(title: @note["title"], content: @note["content"])
+	redirect "/"
 end
 
-get '/notes' do
+get '/notes/:note_id' do
+	@note = Note.find(params[:note_id])
+	erb :note
+end
 
+get '/notes/edit/:note_id' do
+	@note = Note.find(params[:note_id])
+	erb :edit
+end
+
+post '/notes/edit' do
+	@note = params[:note]
+	note = Note.find(@note["id"])
+	note.title = @note["title"]
+	note.content = @note["content"]
+	note.save
+	redirect "/notes/" + @note["id"]
+end
+
+get '/notes/delete/:note_id' do
+	Note.find(params[:note_id]).destroy
+	redirect "/"
 end
